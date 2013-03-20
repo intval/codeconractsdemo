000f150bfcfd0a6579856136464082e3d9f9ff4a
Alex (win) <alex@phpguide.co.il>
2013-03-20 19:39:54 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeContractsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void bla(string s)
+        {
+            // basic check
+            // this check put a lot of garbage in our code
+            if (null == s)
+                throw new ArgumentException("argh");
+        }
+    }
+}
